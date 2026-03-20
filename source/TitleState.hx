e781038f6e131896e28330ab4e0164fdabf48328
idktrolled <camilocarlos1100@gmail.com>
2026-03-19 20:02:00 -0500

    Comment out replay directory creation logic
    Comment out the directory creation code for replays.

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -57,10 +57,10 @@ class TitleState extends MusicBeatState
 		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
 		#end
 		
-		#if sys
+		/*#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
+		#end*/
 
 		@:privateAccess
 		{
